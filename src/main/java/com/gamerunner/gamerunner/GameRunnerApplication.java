3acdd3c4a6d7db87e4717d80e63126065b7e268a
C2dricLeroy <cedric.leroy.psycho@gmail.com>
2023-12-11 14:24:08 +0100

    initial commit

---
+++ src/main/java/com/gamerunner/gamerunner/GameRunnerApplication.java
@@ -0,0 +1,14 @@
+package com.gamerunner.gamerunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRunnerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRunnerApplication.class, args);
+	}
+
+}
